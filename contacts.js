9e1fd0e401bf207d19619a1d14e7d1bce33efe88
Sergio <114320133+sergioing1000@users.noreply.github.com>
2023-08-27 10:41:55 -0500

    modified:   contacts.js

--- contacts.js
+++ contacts.js
@@ -1,7 +1,56 @@
 const fs = require("fs");
 const path = require("path");
 
-// Ejemplo de lectura de un archivo
+const contactsPath = "./db/contacts.json";
+
+const absolutePath = path.resolve(contactsPath);
+console.log("Absolute path:", absolutePath);
+
+const fileName = path.basename(contactsPath);
+console.log("File name:", fileName);
+
+const directoryName = path.dirname(contactsPath);
+console.log("Directory name:", directoryName);
+
+const extension = path.extname(contactsPath);
+console.log("File extension:", extension);
+
+const fullPath = path.join(__dirname, directoryName, fileName);
+console.log("Full path:", fullPath);
+
+
+console.log("******************************************")
+console.log("******************************************");
+console.log("******************************************");
+console.log("******************************************");
+
+console.log("    .__________________________.");
+console.log("    | .___________________. |==|");
+console.log("    | | ................. | |  |");
+console.log("    | | ::::Windows:::::: | |  |");
+console.log("    | | ::::::::::::::::: | |  |");
+console.log("    | | ::::::::::::::::: | |  |");
+console.log("    | | ::::::::::::::::: | |  |");
+console.log("    | | ::::::::::::::::: | |  |");
+console.log("    | | ::::::::::::::::: | | ,|");
+console.log("    | !___________________! |(c|");
+console.log("    !_______________________!__!");
+//console.log("   /                            \");
+//console.log("  /  [][][][][][][][][][][][][]  \");
+//console.log(" /  [][][][][][][][][][][][][][]  \");
+console.log("(  [][][][][____________][][][][]  )");
+console.log(" \ ------------------------------ /");
+console.log("  \______________________________/");
+
+
+
+
+
+
+
+
+
+
 
 function readFile () {
 
@@ -10,15 +59,10 @@ fs.readFile("./db/contacts.json", "utf8", (err, data) => {
     console.error("Error al leer el archivo:", err);
     return;
   }
-  console.log("Contenido del archivo:", data);
+  //console.log("Contenido del archivo:", data);
 });
   
 }
-
-// Ejemplo de obtención de la ruta absoluta de un archivo
-const archivoPath = "archivo.txt";
-const rutaAbsoluta = path.resolve(archivoPath);
-console.log("Ruta absoluta del archivo:", rutaAbsoluta);
 
 module.exports = {
   readFile: readFile,
